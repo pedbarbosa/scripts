36ac02cad3c79ce6b83229a6f190a58c716d0391
pedbarbosa <pedbarbosa@gmail.com>
2014-04-18 19:21:20 +1000

    ffmpeg encoding script

---
+++ encode.bat
@@ -0,0 +1,15 @@
+@ECHO OFF
+SET ffmpeg=C:\Program Files\ffmpeg\bin\ffmpeg.exe
+SET source=D:\Video\encode
+SET destination=D:\Video\incoming
+SET rubbish=D:\Video\rubbish
+SET options=-vcodec libx264 -crf 23 -preset veryslow -acodec libvo_aacenc -ab 128k -ac 2
+
+for %%i in (%source%\*.*) do (
+    "%ffmpeg%" -i "%%i" %options% "%destination%\%%~ni.mkv"
+)
+if %errorlevel% == 0 (
+    move "%destination%\%%~ni.mkv" %rubbish%
+) else (
+    echo Encoding failed :(
+)
