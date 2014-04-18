acb4979df6882ef26fc2fa81e627617a8bea0f49
pedbarbosa <pedbarbosa@gmail.com>
2014-04-18 23:53:40 +1000

    Added XBMC backup script

---
+++ xbmc_backup.bat
@@ -0,0 +1,17 @@
+@ECHO OFF
+
+REM Configurable variables
+SET mysql_user="root"
+SET /P mysql_pass=<xbmc_backup.pwd
+SET mysql_bin="C:\Program Files\MySQL\MySQL Server 5.6\bin"
+SET backup_dir="%userprofile%\Google Drive\Backups\MySQL"
+SET rar_bin="C:\Program Files\WinRAR\rar.exe"
+
+REM Destination for machines set with YYYY-MM-DD
+SET rar_backup=%backup_dir%\xbmc-%date:~0,4%%date:~5,2%%date:~8,2%-%time:~0,2%%time:~3,2%.sql.rar
+
+%mysql_bin%\mysqldump -u %mysql_user% --password=%mysql_pass% -B mymusic32 myvideos75 > %TEMP%\backup.sql
+IF %errorlevel% == 0 (
+    %rar_bin% a -m5 -s %rar_backup% %TEMP%\backup.sql
+    del %TEMP%\backup.sql
+) else echo MySQL dump failed.
