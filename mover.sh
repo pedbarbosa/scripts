f932ccc8822b0fd15085093f8644a78b2085ffbb
Pedro Barbosa <pedbarbosa@gmail.com>
2015-05-05 00:13:28 +1000

    Added more error handling

--- mover.sh
+++ mover.sh
@@ -10,32 +10,46 @@ else
     source ${CONFIGFILE}
 fi
 
+clock () { echo ">> [`date +%H:%M:%S`] "; }
+
 # Check if the script is already running
 if [[ -e ${LOCKFILE} ]] && kill -0 `cat ${LOCKFILE}`; then
-    echo "Refusing to run, found lock file at $LOCKFILE" >> $LOGFILE
-    exit
+    echo `clock`"Refusing to run, found lock file at $LOCKFILE" >> $LOGFILE
+    exit 1
 fi
 
 # Create lock file
 trap "rm -f ${LOCKFILE}; exit" INT TERM EXIT
 echo $$ > ${LOCKFILE}
+echo `clock`"Application successfully started" >> $LOGFILE
 
-# Start logging
-echo "----------------------------" >> $LOGFILE
-echo `date` >> $LOGFILE
-echo "----------------------------" >> $LOGFILE
-
+# Check if rsync is enabled
 if [[ ${RSYNC_ENABLED} -eq "TRUE" ]]; then
-    echo "Invoking rsync..." >> $LOGFILE
+    # Check if any orphaned rsync processes are running
+    pidcheck=$(ps aux | grep [r]sync | wc -l)
+
+    if [ $pidcheck -gt 0 ]; then
+        echo `clock`"Suspending, rsync already running" >> $LOGFILE
+        exit 1
+    fi
+
+    # Rsync execution
+    echo `clock`"Invoking rsync..." >> $LOGFILE
     rsync -av --append --remove-source-files $RSYNC_SOURCE $SOURCE >> $LOGFILE
+    rsync_exit=$?
+    if [ "$rsync_exit" -ne 0 ]; then
+        echo `clock`"Rsync exited with non-zero code, aborting script" >> $LOGFILE
+        exit 1
+    fi
 fi
 
 # Check if remote computer is available
 if [[ ! -d $REMOTEDIR ]]; then
-    echo "Remote computer not available" >> $LOGFILE
+    echo `clock`"Remote computer not available" >> $LOGFILE
     exit 1
 fi
 
+# Move files to final location
 COUNTER=0
 for FILENAME in $(find "$SOURCE/" -maxdepth 1 -type f); do
     FILESIZE="$(stat -c %s $FILENAME)"
@@ -46,9 +60,9 @@ for FILENAME in $(find "$SOURCE/" -maxdepth 1 -type f); do
             mv "$FILENAME" "$NEWNAME"
             FILENAME=$NEWNAME
         fi
-        echo "Moving file $FILENAME" >> $LOGFILE
+        echo `clock`"Moving file $FILENAME" >> $LOGFILE
         mv "$FILENAME" "$DESTINATION/" && let COUNTER+=1
     fi
 done
 
-echo "Moved $COUNTER items" >> $LOGFILE
+echo `clock`"Moved $COUNTER items" >> $LOGFILE
