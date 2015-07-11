ce7f535547eb4acb4385c9f0171518dff0c7bbe4
Pedro Barbosa <pedbarbosa@gmail.com>
2015-07-11 19:12:49 +1000

    Updated error handling for missing password file

--- xbmc_backup.bat
+++ xbmc_backup.bat
@@ -2,10 +2,18 @@
 
 REM Configurable variables
 SET mysql_user="root"
-SET /P mysql_pass=<xbmc_backup.pwd
 SET mysql_bin="C:\Program Files\MySQL\MySQL Server 5.6\bin"
 SET backup_dir="%userprofile%\Google Drive\Backups\MySQL"
 SET rar_bin="C:\Program Files\WinRAR\rar.exe"
+
+REM Check for password file
+IF NOT EXIST xbmc_backup.pwd (
+    ECHO ERROR: Missing password file!
+    ECHO Please create a file on script directory called "xbmc_backup.pwd".
+    ECHO This file should *ONLY* contain the password for the %mysql_user% MySQL user.
+    GOTO :EXIT
+)
+SET /P mysql_pass=<xbmc_backup.pwd
 
 REM Destination for machines set with YYYY-MM-DD
 SET hour=%time: =0%
@@ -14,5 +22,7 @@ SET rar_backup=%backup_dir%\xbmc-%date:~0,4%%date:~5,2%%date:~8,2%-%hour:~0,2%%t
 %mysql_bin%\mysqldump -u %mysql_user% --password=%mysql_pass% -B mymusic48 myvideos90 > %TEMP%\backup.sql
 IF %errorlevel% == 0 (
     %rar_bin% a -m5 -s %rar_backup% %TEMP%\backup.sql
-    del %TEMP%\backup.sql
-) else echo MySQL dump failed.
+    DEL %TEMP%\backup.sql
+) ELSE ECHO MySQL dump failed.
+
+:EXIT
