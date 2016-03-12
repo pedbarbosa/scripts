f3fa0351054e8d96369956d800e412f57a6ad822
Pedro Barbosa <pedbarbosa@gmail.com>
2016-03-12 18:34:56 +1000

    Updated script for DD-MM-YYYY

--- xbmc_backup.bat
+++ xbmc_backup.bat
@@ -15,9 +15,10 @@ IF NOT EXIST xbmc_backup.pwd (
 )
 SET /P mysql_pass=<xbmc_backup.pwd
 
-REM Destination for machines set with YYYY-MM-DD
 SET hour=%time: =0%
-SET rar_backup=%backup_dir%\xbmc-%date:~0,4%%date:~5,2%%date:~8,2%-%hour:~0,2%%time:~3,2%.sql.rar
+REM Backup destination set for DD-MM-YYYY, second option is for YYYY-MM-DD
+SET rar_backup=%backup_dir%\kodi-%date:~10,4%%date:~7,2%%date:~4,2%-%hour:~0,2%%time:~3,2%.sql.rar
+REM SET rar_backup=%backup_dir%\xbmc-%date:~0,4%%date:~5,2%%date:~8,2%-%hour:~0,2%%time:~3,2%.sql.rar
 
 %mysql_bin%\mysqldump -u %mysql_user% --password=%mysql_pass% -B mymusic52 myvideos93 > %TEMP%\backup.sql
 IF %errorlevel% == 0 (
