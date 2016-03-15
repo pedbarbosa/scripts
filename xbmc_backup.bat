f2cd01c2c2aee4cfd498695e810f15059ecc943c
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2016-03-16 04:18:55 +1000

    Updated XBMC databases for 16.0 Jarvis

--- xbmc_backup.bat
+++ xbmc_backup.bat
@@ -20,7 +20,7 @@ REM Backup destination set for DD-MM-YYYY, second option is for YYYY-MM-DD
 SET rar_backup=%backup_dir%\kodi-%date:~10,4%%date:~7,2%%date:~4,2%-%hour:~0,2%%time:~3,2%.sql.rar
 REM SET rar_backup=%backup_dir%\xbmc-%date:~0,4%%date:~5,2%%date:~8,2%-%hour:~0,2%%time:~3,2%.sql.rar
 
-%mysql_bin%\mysqldump -u %mysql_user% --password=%mysql_pass% -B mymusic52 myvideos93 > %TEMP%\backup.sql
+%mysql_bin%\mysqldump -u %mysql_user% --password=%mysql_pass% -B mymusic56 myvideos99 > %TEMP%\backup.sql
 IF %errorlevel% == 0 (
     %rar_bin% a -m5 -s %rar_backup% %TEMP%\backup.sql
     DEL %TEMP%\backup.sql
