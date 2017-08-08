0bf3fbd1e7248cc189d28f1dc75764b717058335
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2017-08-08 23:11:26 +1000

    Added script for finding and copying new files

---
+++ cp_latest_unpack.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+INTERVAL=$1
+SOURCE=~/downloads/
+DESTINATION=~/unpack/
+EXTENSION="*.mkv"
+
+SEARCH=$(find $SOURCE -type f -name $EXTENSION -cmin -$INTERVAL | xargs -i basename "{}")
+SEARCH_EXIT=$?
+
+if [[ "$SEARCH_EXIT" == 0 ]]
+then
+    echo
+    echo "Found the files listed below:"
+    echo
+    echo "$SEARCH"
+    echo
+    read -r -p "Would you like to copy these files to $DESTINATION ? [y/N] " response
+    if [[ "$response" =~ ^([yY][eE][sS]|[yY])+$ ]]
+    then
+        echo
+        echo "Copying files to $DESTINATION..."
+        find $SOURCE -type f -name $EXTENSION -cmin -$INTERVAL | xargs -i cp {} $DESTINATION
+        if [[ "$?" == 0 ]]
+        then
+            echo "Successfully copied files."
+        else
+            echo "Copy process failed."
+        fi
+        echo
+        echo "Listing contents of $DESTINATION:"
+        ls -l $DESTINATION
+    fi
+else
+    echo "There were no .mkv files created within the last $INTERVAL minutes."
+fi
