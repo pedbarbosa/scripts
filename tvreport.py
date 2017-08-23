b5a2067d191939dc0db0a4e5473c64c262bd2215
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2017-08-23 19:55:23 +1000

    Refactoring

--- tvreport.py
+++ tvreport.py
@@ -4,12 +4,66 @@ import os
 import pickle
 import importlib
 
+
+def exit_with_msg(m):
+    print(m)
+    exit(1)
+
+
+def read_pickle(f):
+    with open(f, 'rb') as handle:
+        return pickle.load(handle)
+
+
+def update_pickle(f, d):
+    with open(f, 'wb') as handle:
+        pickle.dump(d, handle)
+
+
+def track_codec(i):
+    if i.format == 'HEVC':
+        return 'x265'
+    elif i.format == 'AVC':
+        return 'x264'
+    elif i.format == 'MPEG-4 Visual' or i.format == 'MPEG Video':
+        return 'mpeg'
+    else:
+        return ''
+
+
+def track_resolution(i):
+    if i.height >= 800:
+        return '1080p'
+    elif 640 <= i.height < 800:
+        return '720p'
+    elif i.height < 640:
+        return 'sd'
+    else:
+        return ''
+
+
+def episode_badge(i, n):
+    if (i['x265_1080p'] + i['x264_1080p']) == n:
+        return '1080p'
+    elif (i['x265_720p'] + i['x264_720p'] + i['mpeg_720p']) == n:
+        return '720p'
+    elif (i['x265_sd'] + i['x264_sd'] + i['mpeg_sd']) == n:
+        return 'SD'
+    else:
+        return 'Mix'
+
+
 if not importlib.util.find_spec('progressbar'):
     exit_with_msg('Error: Python module not found. Install progressbar2.')
 else:
     import progressbar
 
+# Variable initialisation
+scan_directory = html_file = report_html = recode_html = pickle_file = recode = ''
+codec_override = video_extensions = []
+episodes = dict()
 config_file = os.path.expanduser('~') + '/.tvreport'
+
 # Check configuration file
 if not os.path.isfile(config_file):
     exit_with_msg('Error: Config file %s missing. Copy tvreport.cfg to %s and configure as required.' % (config_file, config_file))
@@ -19,59 +73,23 @@ else:
 if not os.path.isdir(scan_directory):
     exit_with_msg('Error: %s is not a directory or doesn\'t exist. Check your %s config file.' % (scan_directory, config_file))
 
-
-def exit_with_msg(message):
-    print(message)
-    exit(1)
-
-
-def update_pickle(dictionary):
-    with open(pickle_file, 'wb') as handle:
-        pickle.dump(dictionary, handle)
-
-
-def track_codec(track):
-    if track.format == 'HEVC':
-        return 'x265'
-    elif track.format == 'AVC':
-        return 'x264'
-    elif track.format == 'MPEG-4 Visual' or track.format == 'MPEG Video':
-        return 'mpeg'
-    else:
-        return ''
-
-
-def track_resolution(track):
-    if track.height >= 800:
-        return '1080p'
-    elif track.height >= 640 and track.height < 800:
-        return '720p'
-    elif track.height < 640:
-        return 'sd'
-    else:
-        return ''
-
-
 # Checking if a scan has been run previously
 if os.path.isfile(pickle_file):
-    with open(pickle_file, 'rb') as handle:
-        episodes = pickle.load(handle)
-        # Check if previously scanned files have been deleted
-        episodes_pickle = len(episodes)
-        print('Found %s episodes from previous scan(s) cached, scanning for removed episodes...' % episodes_pickle)
-        scan_bar_progress = 0
-        scan_bar = progressbar.ProgressBar(max_value=episodes_pickle)
-        for episode_path in list(episodes):
-            scan_bar_progress += 1
-            if not os.path.exists(episode_path):
-                del episodes[episode_path]
-            scan_bar.update(scan_bar_progress)
-        scan_bar.update(episodes_pickle)
-        print('\nFinished existing file scan, %s episodes removed.' % (episodes_pickle - len(episodes)))
-    update_pickle(episodes)
+    episodes = read_pickle(pickle_file)
+    # Check if previously scanned files have been deleted
+    episodes_pickle = len(episodes)
+    print('Found %s episodes from previous scan(s) cached, scanning for removed episodes...' % episodes_pickle)
+    scan_bar_progress = 0
+    scan_bar = progressbar.ProgressBar(max_value=episodes_pickle)
+    for episode_path in list(episodes):
+        scan_bar_progress += 1
+        if not os.path.exists(episode_path):
+            del episodes[episode_path]
+        scan_bar.update(scan_bar_progress)
+    scan_bar.update(episodes_pickle)
+    print('\nFinished existing file scan, %s episodes removed.' % (episodes_pickle - len(episodes)))
+    update_pickle(pickle_file, episodes)
     check_episodes = dict(episodes)
-else:
-    episodes = dict()
 shows = dict()
 errors = dict()
 
@@ -80,8 +98,6 @@ episodes_directories = len(next(os.walk(scan_directory))[1])
 scan_bar_progress = 0
 scan_bar = progressbar.ProgressBar(max_value=episodes_directories)
 print('Found %s directories, scanning for episodes...' % episodes_directories)
-
-recode = ''
 
 for dirpath, dirnames, filenames in os.walk(scan_directory, topdown=True):
     scan_bar.update(scan_bar_progress)
@@ -129,14 +145,17 @@ for dirpath, dirnames, filenames in os.walk(scan_directory, topdown=True):
                 else:
                     episode_format = episodes[episode_path]['codec'] + '_' + episodes[episode_path]['height']
 
+                # Process shows that have been marked as 'overridden'
                 if videodir in codec_override:
-                    #print('OVERRIDDEN - Found file %s belonging to show %s, changing codec to x265...' % (episode_path, videodir))
                     episodes[episode_path]['codec'] = 'x265'
                     episode_format = episodes[episode_path]['codec'] + '_' + episodes[episode_path]['height']
 
+                # Create a new empty show entry
                 if videodir not in shows:
-                    shows[videodir] = {'show_size': 0, 'x265_1080p': 0, 'x265_720p': 0, 'x265_sd': 0, 'x264_1080p': 0, 'x264_720p': 0, 'x264_sd': 0, 'mpeg_720p': 0, 'mpeg_sd': 0}
+                    shows[videodir] = {'show_size': 0, 'x265_1080p': 0, 'x265_720p': 0, 'x265_sd': 0, 'x264_1080p': 0,
+                                       'x264_720p': 0, 'x264_sd': 0, 'mpeg_720p': 0, 'mpeg_sd': 0}
 
+                # Check for valid mediainfo details
                 if len(episode_format) < 6:
                     errors[episode_path] = 'Could not obtain video details'
                 else:
@@ -146,13 +165,13 @@ for dirpath, dirnames, filenames in os.walk(scan_directory, topdown=True):
                 # Testing x265 report
                 if episodes[episode_path]['codec'] != 'x265':
                     recode += '<tr><td align=center>%s</td><td align=center>%s</td><td align=right>%s</td><td>%s</td></tr>' % (episodes[episode_path]['codec'], episodes[episode_path]['height'], episodes[episode_path]['size'], episode_path)
-                
+
                 # Ensure files have been properly processed
                 if episode_path in check_episodes:
                     del check_episodes[episode_path]
 
     # Update pickle file at the end of each directory
-    update_pickle(episodes)
+    update_pickle(pickle_file, episodes)
 # End of root directory scan
 scan_bar.update(episodes_directories)
 
@@ -183,16 +202,13 @@ table.sortable th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sor
         total_episodes += num_episodes
         show_size = int(details['show_size'] / 1024 / 1024)
         total_size += show_size
-        if (details['x265_1080p'] + details['x264_1080p']) == num_episodes:
-            show_badge = '1080p'
-        elif (details['x265_720p'] + details['x264_720p'] + details['mpeg_720p']) == num_episodes:
-            show_badge = '720p'
-        elif (details['x265_sd'] + details['x264_sd'] + details['mpeg_sd']) == num_episodes:
-            show_badge = 'SD'
-        else:
-            show_badge = 'Mix'
-        report.write('<tr><td class="left">%s</td><td>%s</td><td class="center"><progress max="%s" value="%s"></progress></td><td>%s</td><td>%s</td>' % (show, show_size, num_episodes, x265_episodes, num_episodes, show_badge))
-        report.write('<td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>' % (details['x265_1080p'], details['x265_720p'], details['x265_sd'], details['x264_1080p'], details['x264_720p'], details['x264_sd'], details['mpeg_720p'], details['mpeg_sd']))
+        show_badge = episode_badge(details, num_episodes)
+        report.write('<tr><td class="left">%s</td><td>%s</td><td class="center"><progress max="%s" value="%s"> \
+            </progress></td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td> \
+            <td>%s</td><td>%s</td></tr>' % (
+            show, show_size, num_episodes, x265_episodes, num_episodes, show_badge, details['x265_1080p'],
+            details['x265_720p'], details['x265_sd'], details['x264_1080p'], details['x264_720p'], details['x264_sd'],
+            details['mpeg_720p'], details['mpeg_sd']))
     total_stats = 'Scanned %s shows with %s episodes (%s in x265 format - %.2f%%). %.0f GB in total' % (episodes_directories, total_episodes, total_x265, ((total_x265*100)/total_episodes), (total_size/1024))
     report.write('</table><br><table id="shows"><th>%s</th></table></body></html>' % (total_stats))
 
@@ -201,7 +217,7 @@ if len(errors) > 0:
     for messages, values in sorted(errors.items()):
         print('%s - %s' % (messages, values))
 
-with open(recode_html, 'w') as handle:
-    handle.write('<table border=1>%s</table>' % (recode))
+with open(recode_html, 'w') as handle_html:
+    handle_html.write('<table border=1>%s</table>' % recode)
 
 print('\nFinished full directory scan. %s' % total_stats)
