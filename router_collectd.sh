efe7010ed091e655cd595a5c5d52ef53c8175570
Pedro Barbosa <pedbarbosa@users.noreply.github.com>
2021-10-22 10:58:03 +1000

    Added script for checking router log age and restart collectd if required

---
+++ router_collectd.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Set collectd RRD path
+SCAN_PATH=/var/lib/collectd/rrd/router
+
+# Set maximum RRD file age, in minutes
+MAX_AGE=10
+
+LAST_MODIFIED=$(find $SCAN_PATH -type f -printf '%T@ %p\n' | sort -n | tail -1 | cut -f2- -d" ")
+LAST_TIMESTAMP=$(date +%s -r $LAST_MODIFIED)
+
+CURRENT_TIME=$(date +%s)
+DELTA=$((CURRENT_TIME-LAST_TIMESTAMP))
+
+SECONDS=$(($MAX_AGE*60))
+if [[ ${DELTA} -gt ${SECONDS} ]]; then
+    echo "Newest update in '${SCAN_PATH}' was ${DELTA} seconds ago, which exceeds the threshold of ${SECONDS}."
+    echo "Stopping 'collectd' in router..."
+    ssh router '/etc/init.d/collectd stop'
+    sleep 1
+    echo "Starting 'collectd' in router..."
+    ssh router '/etc/init.d/collectd start'
+fi
